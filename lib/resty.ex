88f9bde7be7441ff9ca8985d861698ba2aea88c3
Ayman Osman <aymano.osman@gmail.com>
2016-02-02 15:14:58 +0000

    rest

--- lib/resty.ex
+++ lib/resty.ex
@@ -1,16 +1,29 @@
 defmodule Resty do
-  @behaviour :cowboy_websocket_handler
 
-	def init(req, opts) do
-    {:cowboy_rest, req, opts}
+	def init({:tcp, :http}, _req, _opts) do
+    {:upgrade, :protocol, :cowboy_rest}
   end
 
-  def handle(req, state) do
-    {:ok, reply} = :cowboy_req.reply(
-      200,
-      [{"content-type", "text/html"}],
-      "<h1>Hello Erlang</h1>",
-      req)
-    {:ok, reply, state}
+  def content_types_provided(req, state) do
+    IO.puts "lol wut"
+    # mappings = [
+    #   {"application/json", &aaa_to_json/2}
+    # ]
+    mappings = [
+      {{<<"text">>, <<"html">>, '*'}, &to_html/2}
+    ]
+    {mappings, req, state}
   end
+
+  def to_html(req, state) do
+    {"<h1>lol</h1>", req, state}
+  end
+
+  def aaa_to_json(req, state) do
+    body = """
+    {"rest": "Hello Cowboy!"}
+    """
+    {body, req, state}
+  end
+
 end
