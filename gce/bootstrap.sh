c324172c199cb36e1fa9db04d39508a6c2ac6b9d
Ayman Osman <aymano.osman@gmail.com>
2016-03-31 00:11:28 +0100

    wip

--- gce/bootstrap.sh
+++ gce/bootstrap.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 mkdir -p /code
 cd /code
@@ -9,25 +10,27 @@ tar -xzf carl.tar.gz
 echo Writing short nodename of carl into vm.args
 cat > releases/0.0.1/vm.args<<EOF
 -sname carl
-
 -setcookie carl
 EOF
 
+echo Setting HOME to ubuntu, for some reason erlexec complains about this???
+export HOME=/home/ubuntu
 hostserlang=~/.hosts.erlang
-echo What is in .hosts.erlang??
-cat $hostserlang
+echo What is in .hosts.erlang? $hostserlang
+cat $hostserlang || true
 
 cat > $hostserlang<<EOF
 'carl@joe'.
 'carl@mike'.
 EOF
 
+cat $hostserlang || true
 
-
-echo Setting HOME to ubuntu, for some reason erlexec complains about this???
-echo Old HOME = $HOME
-export HOME=/home/ubuntu
 echo "Caaarrrlll"
 bin/carl start
 echo Calling bin/carl rpc net_adm world
-bin/carl rpc net_adm world
+bin/carl rpc net_adm host_file
+bin/carl rpc net_adm world # Why doesn't this work???
+bin/carl rpc net_adm ping carl@joe.
+bin/carl rpc net_adm ping carl@mike.
+bin/carl rpc erlang nodes
