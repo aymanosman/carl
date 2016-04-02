bf10db5084bdbada238394f53a4af1c22a3d6655
Ayman Osman <aymano.osman@gmail.com>
2016-04-02 02:19:14 +0100

    wip

--- gce/bootstrap-kubernetes.sh
+++ gce/bootstrap-kubernetes.sh
@@ -3,5 +3,7 @@ set -e
 
 curl -O https://storage.googleapis.com/kubernetes-release/release/v1.2.0/bin/linux/amd64/kubectl
 chmod +x kubectl
-pods=$(./kubectl get pod -lapp=carl -o=jsonpath={.items..metadata.name})
-# for p in pods; describe pod {.ip}
+podIPs=$(./kubectl get pod -lapp=carl -o=jsonpath={.items..status.podIP})
+echo $podIPs
+# ping them all
+
